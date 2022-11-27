0d4c20bbc20b3ca616a017554d630d3b9b53d716
Layton Whiteley <layton.w@smart-host.com>
2022-11-27 21:05:25 +0100

    fix: replace icon package

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,34 +1,35 @@
-import { AiFillGithub, AiFillLinkedin, AiOutlineTwitter } from 'react-icons/ai';
-import { NpmLogo } from './npm.svg';
+import {
+  Linkedin,
+  Npm,
+  Twitter,
+  Github,
+  Devdotto,
+} from '@icons-pack/react-simple-icons';
 
+const iconSize = 32;
 const links = [
   {
-    icon: <AiFillGithub />,
+    icon: <Github size={iconSize} />,
     title: 'Github',
     link: 'https://github.com/lwhiteley',
   },
   {
-    icon: <AiFillLinkedin />,
+    icon: <Linkedin size={iconSize} />,
     title: 'Linkedin',
     link: 'https://jm.linkedin.com/in/layton-whiteley-25284541',
   },
   {
-    icon: <AiOutlineTwitter />,
+    icon: <Twitter size={iconSize} />,
     title: 'Twitter',
     link: 'https://twitter.com/LaytonGates',
   },
   {
-    icon: <NpmLogo />,
+    icon: <Npm size={iconSize} />,
     title: 'npm',
     link: 'https://npmjs.com/~lwhiteley',
   },
   {
-    icon: (
-      <img
-        src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"
-        width="48px"
-      />
-    ),
+    icon: <Devdotto size={iconSize} />,
     title: 'Dev Community',
     link: 'https://dev.to/lwhiteley',
   },
@@ -36,7 +37,7 @@ const links = [
 
 export const Links = () => {
   return (
-    <div class="flex space-x-6 justify-center">
+    <div className="flex space-x-6 justify-center">
       {links.map(({ icon, link, title }) => {
         return (
           <a
@@ -44,7 +45,7 @@ export const Links = () => {
             alt={title}
             target="_blank"
             href={link}
-            class="text-5xl"
+            className="text-5xl"
           >
             {icon || title}
           </a>
